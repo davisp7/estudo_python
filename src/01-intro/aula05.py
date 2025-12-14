498e26ebe5788fa569bcb83fc51f671a81f16669
davisp7 <davipinheirosilva@gmail.com>
2025-12-14 15:53:54 -0300

    add aula05.py with examples of data types including numbers, strings, booleans, lists, tuples, sets, and dictionaries

---
+++ src/01-intro/aula05.py
@@ -0,0 +1,92 @@
+""" Aula 04 - Tipos de Dados"""
+
+# Numericos
+# int, float
+
+idade = 20
+peso = 70.5
+
+print(idade, type(idade))
+print(peso, type(peso))
+
+# Strings
+
+nome = 'Pedro'
+sobrenome = 'dos Santos'
+nome_completo = nome + ' ' + sobrenome
+print(nome_completo)
+
+produto = 'Coca-Cola'
+
+print(f'O cliente {nome} {sobrenome} comprou o produto {produto}')
+
+# O cliente nome_completo comprou o produto produto
+
+print(nome[0], nome[-1])
+
+print(nome.lower())
+print(nome.upper())
+
+print(1, 3, 7, 10, 'desadsa', sep = '  ')
+
+# Boolean
+
+ligado = True
+print(ligado, type(ligado))
+
+resultado = 10 == 10
+
+print(resultado, type(resultado))
+
+# Listas
+
+frutas = ['banana', 'uva', 'morango']
+print(frutas)
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+# print(frutas[3])
+
+frutas[0] = 'banana nanica'
+frutas.append('abacaxi')
+print(frutas)
+print(len(frutas))
+
+for fruta in frutas:
+    print(fruta.upper())
+
+# Tuplas
+
+codigos = ('SP01', 'SP02', 'SP03')
+print(codigos)
+print(codigos[0])
+
+# codigos[0] = 'SP10'  # TypeError, tuplas são imutáveis
+print(len(codigos))
+
+# Conjuntos(sets)
+
+resultado_sorteio = {10, 4, 3, 55, 9}
+print(resultado_sorteio)
+
+resultado_sorteio.add(23)
+print(resultado_sorteio)
+
+# Dicionarios
+
+funcionario ={
+    'codigo': 123,
+    'nome': 'Maria da Silva',
+    'salario': 7000.00
+}
+
+print(funcionario)
+print(funcionario['codigo'])
+print(funcionario['nome'])
+print(funcionario['salario'])
+
+print(funcionario.keys())
+print(funcionario.values())
+
+funcionario['salario'] = 9000.00
+print(funcionario)
