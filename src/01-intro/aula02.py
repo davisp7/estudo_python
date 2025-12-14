ead07532c8b0c53c5ff096340ecb49435b288258
davisp7 <davipinheirosilva@gmail.com>
2025-12-14 12:46:22 -0300

    add aula02.py with examples of reserved words, identifiers, and best practices

---
+++ src/01-intro/aula02.py
@@ -0,0 +1,38 @@
+# Palvras reservadas
+# True False, None, and, import, lambda
+
+# Identificadores
+# Nomes dados para variáveis, funções, classes, etc.
+# case sensitive
+# devem iniciar com uma letra ou _
+# nao pode ter espaços em branco
+# nao pode usar caracteres especiais como @, $, %, etc.
+
+nome = 'Maria'
+idade = 30
+
+Nome = 'João'
+
+nome_completo = 'Maria da Silva'
+
+# Boas praticas(Clean code)
+
+c = 10
+contador = 10
+
+s = 10 + 20
+soma = 10 + 20
+
+# Constantes
+
+PI = 3.14
+
+idade = 18
+MAIORIDADE = 18
+
+
+if idade >= MAIORIDADE:
+
+    print('Maior de idade')
+else:
+    print('Menor de idade')
